466254a377fadb97150d701224489877c729e9ca
Nan Yu <nanyu@google.com>
2022-10-03 19:05:21 +0000

    Force to enable client-side throttling for mono-repo mode (#169)
    https://github.com/GoogleContainerTools/kpt-config-sync/commit/732f94ea702a9a2f97335f0000306f4e17b363ee
    disabled client-side throttling, which then caused the monorepo CI jobs
    to start becoming flaky and then constantly failing.
    
    This commit forces to enable the client-side throttling for mono-repo
    only. Compared with 1.12.x releases, the client-side throttling QPS was
    increased from 20 (burst: 40) to 30 (burst: 60).

--- cmd/nomos/parse/parse.go
+++ cmd/nomos/parse/parse.go
@@ -45,7 +45,7 @@ func GetSyncedCRDs(ctx context.Context, skipAPIServer bool) ([]*v1beta1.CustomRe
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	config, err := restconfig.NewRestConfig(restconfig.DefaultTimeout)
+	config, err := restconfig.NewRestConfigWithThrottling(restconfig.DefaultTimeout)
 	if err != nil {
 		return nil, getSyncedCRDsError(err, "failed to create rest config")
 	}

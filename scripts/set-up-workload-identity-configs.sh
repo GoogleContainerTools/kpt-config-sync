70229fc5d725dd0b4cc26255e0790f7bdb926f5c
Sam Dowell <sdowell@google.com>
2023-01-05 14:48:10 -0800

    update provisioning scripts for multi-project (#338)
    - move make targets to Makefile.oss.prow for discoverability
    - update WI script to support provisioning in separate project

--- scripts/set-up-workload-identity-configs.sh
+++ scripts/set-up-workload-identity-configs.sh
@@ -18,10 +18,10 @@ set -euo pipefail
 # This script sets up the test-infra for validating the authentication via workload identity.
 #
 # Pre-requisite:
-# 1. GCP_PROJECT and FLEET_HOST_PROJECT are set.
+# 1. GCP_PROJECT, PROW_PROJECT, and FLEET_HOST_PROJECT are set.
 # 2. The FLEET_HOST_PROJECT project is created.
 # 4. You have the permission to enable API service in the ${GCP_PROJECT} project.
-# 5. You have permission to manage IAM service accounts and policies in both the ${GCP_PROJECT} project and the ${FLEET_HOST_PROJECT} project.
+# 5. You have permission to manage IAM service accounts and policies in the ${GCP_PROJECT}, ${PROW_PROJECT}, and ${FLEET_HOST_PROJECT} projects.
 
 if [ "${GCP_PROJECT:-"unset"}" == "unset" ]; then
   echo 'Error: GCP_PROJECT is unset. Please provide the GCP_PROJECT env variable to specify your GCP project' >&2
@@ -30,6 +30,12 @@ fi
 
 if [ "${FLEET_HOST_PROJECT:-"unset"}" == "unset" ]; then
   echo 'Error: FLEET_HOST_PROJECT is unset. Please provide the FLEET_HOST_PROJECT env variable to specify your fleet host project' >&2
+  exit 1
+fi
+
+# PROW_PROJECT is the project where the prow build clusters
+if [ "${PROW_PROJECT:-"unset"}" == "unset" ]; then
+  echo 'Error: PROW_PROJECT is unset. Please provide the PROW_PROJECT env variable to specify your prow project' >&2
   exit 1
 fi
 
@@ -68,5 +74,5 @@ gcloud iam service-accounts add-iam-policy-binding --project="${GCP_PROJECT}" \
 
 echo "Granting the e2e-test-runner service account of the {GCP_PROJECT} project the 'roles/gkehub.admin' role for the '${FLEET_HOST_PROJECT}' project"
 gcloud projects add-iam-policy-binding "${FLEET_HOST_PROJECT}" \
-  --member "serviceAccount:e2e-test-runner@${GCP_PROJECT}.iam.gserviceaccount.com" \
+  --member "serviceAccount:e2e-test-runner@${PROW_PROJECT}.iam.gserviceaccount.com" \
   --role roles/gkehub.admin

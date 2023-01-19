70c602f0974e2790bd13cee488a39c0059195284
Karl Isenberg <karlisenberg@google.com>
2023-01-19 11:12:22 -0800

    Add log.AsYAML and log.AsYAMLDiff (#362)
    - Use the Stringer pattern (like the existing log.AsJSON) to avoid
      expensive diffing and serializing of objects, for logging with
      configurable verbosity.
    - Use github.com/kylelemons/godebug/diff for diffing, because it
      doesn't truncate the way cmp.Diff does.
    - Use the (configurable) scheme to lookup object GKV from its type
      and encode YAML, if possible. Fallback to yaml.Marshal for other
      Go types.
    
    Change-Id: Idbf2aaac522eae5dd775523b1f07fd0e8e1b0d04

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/google/go-containerregistry v0.11.0
 	github.com/google/uuid v1.3.0
 	github.com/jstemmer/go-junit-report/v2 v2.0.0
+	github.com/kylelemons/godebug v1.1.0
 	github.com/open-policy-agent/cert-controller v0.5.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.12.1
--- pkg/util/log/stringer.go
+++ pkg/util/log/stringer.go
@@ -17,7 +17,14 @@ package log
 import (
 	"fmt"
 
+	"github.com/kylelemons/godebug/diff"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	jserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/apimachinery/pkg/util/json"
+	"kpt.dev/configsync/pkg/core"
+	"kpt.dev/configsync/pkg/kinds"
+	"sigs.k8s.io/yaml"
 )
 
 type jsonStringer struct {
@@ -31,11 +38,103 @@ func AsJSON(o interface{}) fmt.Stringer {
 	return &jsonStringer{O: o}
 }
 
-// Returns the object as json, or the error string if marshalling fails.
+// String returns the object as json, or the error string if marshalling fails.
 func (ojs *jsonStringer) String() string {
 	bytes, err := json.Marshal(ojs.O)
 	if err != nil {
 		return err.Error()
 	}
 	return string(bytes)
+}
+
+type yamlStringer struct {
+	O      interface{}
+	Scheme *runtime.Scheme
+}
+
+// AsYAML returns a new stringer object that delays marshaling until the
+// String method is called. For logging at higher verbosity levels, to
+// avoid formatting when the output isn't going to be used.
+// The primary use is for logging Kubernetes objects, but should also work
+// with other types, like Go structs.
+func AsYAML(o interface{}) fmt.Stringer {
+	return &yamlStringer{O: o}
+}
+
+// AsYAMLWithScheme is similar to AsYAML, except it allows specifying which
+// scheme to use to encode the object, instead of defaulting to the global
+// `core.Scheme`.
+func AsYAMLWithScheme(obj runtime.Object, scheme *runtime.Scheme) fmt.Stringer {
+	return &yamlStringer{O: obj, Scheme: scheme}
+}
+
+// String returns the object as yaml, or the error string if marshalling fails.
+func (oys *yamlStringer) String() string {
+	// Use scheme-aware serialization, if possible.
+	// This adds type fields and orders consistently.
+	if rObj, ok := oys.O.(runtime.Object); ok {
+		scheme := oys.Scheme
+		// Default to the global scheme, if unspecified
+		if scheme == nil {
+			scheme = core.Scheme
+		}
+		// Make best effort to ensure GVK is set
+		_, isUnstructured := rObj.(*unstructured.Unstructured)
+		if !isUnstructured && rObj.GetObjectKind().GroupVersionKind().Empty() {
+			gvk, err := kinds.Lookup(rObj, scheme)
+			// do nothing if lookup errors
+			if err == nil {
+				// copy the object to avoid side effects
+				rObj = rObj.DeepCopyObject()
+				rObj.GetObjectKind().SetGroupVersionKind(gvk)
+			}
+		}
+		// Encode
+		yamlSerializer := jserializer.NewYAMLSerializer(jserializer.DefaultMetaFactory, scheme, scheme)
+		bytes, err := runtime.Encode(yamlSerializer, rObj)
+		if err != nil {
+			return err.Error()
+		}
+		return string(bytes)
+	}
+	// Default to general yaml serializer
+	bytes, err := yaml.Marshal(oys.O)
+	if err != nil {
+		return err.Error()
+	}
+	return string(bytes)
+}
+
+type yamlDiffStringer struct {
+	Old, New interface{}
+	Scheme   *runtime.Scheme
+}
+
+// AsYAMLDiff returns a new stringer object that delays marshaling and diffing
+// until the String method is called. For logging at higher verbosity levels, to
+// avoid formatting when the output isn't going to be used.
+// The primary use is for comparing two Kubernetes objects, but should also work
+// with other types, like Go structs.
+// Does not do any object type or version conversion.
+func AsYAMLDiff(old, new interface{}) fmt.Stringer {
+	return &yamlDiffStringer{Old: old, New: new}
+}
+
+// AsYAMLDiffWithScheme is similar to AsYAMLDiff, except it allows specifying
+// which scheme to use to encode the objects, instead of defaulting to the
+// global `core.Scheme`.
+func AsYAMLDiffWithScheme(old, new runtime.Object, scheme *runtime.Scheme) fmt.Stringer {
+	return &yamlDiffStringer{Old: old, New: new, Scheme: scheme}
+}
+
+// String returns a diff (- Removed, + Added) of the objects as yaml, or the
+// error string if marshalling fails.
+// Uses diff.Diff to print full yaml, instead of cmp.Diff which truncates.
+func (yds *yamlDiffStringer) String() string {
+	if yds.Scheme != nil {
+		return diff.Diff(
+			AsYAMLWithScheme(yds.Old.(runtime.Object), yds.Scheme).String(),
+			AsYAMLWithScheme(yds.New.(runtime.Object), yds.Scheme).String())
+	}
+	return diff.Diff(AsYAML(yds.Old).String(), AsYAML(yds.New).String())
 }

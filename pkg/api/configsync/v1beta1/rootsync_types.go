6312c9821f5d77583dc5ac064bddcb464d61479f
Sam Dowell <sdowell@google.com>
2023-03-13 12:11:23 -0700

    feat: add notification configuration API (#463)
    This introduces the RSync API for configuring notifications. The
    implementation of this API will be submitted as a subsequent change.

--- pkg/api/configsync/v1beta1/rootsync_types.go
+++ pkg/api/configsync/v1beta1/rootsync_types.go
@@ -77,6 +77,11 @@ type RootSyncSpec struct {
 	// +nullable
 	// +optional
 	Override *OverrideSpec `json:"override,omitempty"`
+
+	// notification specifies references to the notification configs.
+	// +nullable
+	// +optional
+	NotificationConfig *NotificationConfig `json:"notificationConfig,omitempty"`
 }
 
 // RootSyncStatus defines the observed state of RootSync

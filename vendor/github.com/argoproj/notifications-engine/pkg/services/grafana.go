b4593c4ecdd014770e01ecdd14c7062d8b7b8681
Sam Dowell <sdowell@google.com>
2023-03-14 16:04:10 -0700

    feat: implement notification image (#468)
    Implement a notification image using the Argo notification-engine
    library. The notification controller is configured to watch either a
    RootSync or RepoSync in a single namespace.

---
+++ vendor/github.com/argoproj/notifications-engine/pkg/services/grafana.go
@@ -0,0 +1,92 @@
+package services
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+	"time"
+
+	httputil "github.com/argoproj/notifications-engine/pkg/util/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type GrafanaOptions struct {
+	ApiUrl             string `json:"apiUrl"`
+	ApiKey             string `json:"apiKey"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify"`
+}
+
+type grafanaService struct {
+	opts GrafanaOptions
+}
+
+func NewGrafanaService(opts GrafanaOptions) NotificationService {
+	return &grafanaService{opts: opts}
+}
+
+type GrafanaAnnotation struct {
+	Time     int64    `json:"time"` // unix ts in ms
+	IsRegion bool     `json:"isRegion"`
+	Tags     []string `json:"tags"`
+	Text     string   `json:"text"`
+}
+
+func (s *grafanaService) Send(notification Notification, dest Destination) error {
+	ga := GrafanaAnnotation{
+		Time:     time.Now().Unix() * 1000, // unix ts in ms
+		IsRegion: false,
+		Tags:     strings.Split(dest.Recipient, "|"),
+		Text:     notification.Message,
+	}
+
+	if notification.Message == "" {
+		log.Warnf("Message is an empty string or not provided in the notifications template")
+	}
+
+	client := &http.Client{
+		Transport: httputil.NewLoggingRoundTripper(
+			httputil.NewTransport(s.opts.ApiUrl, s.opts.InsecureSkipVerify), log.WithField("service", "grafana")),
+	}
+
+	jsonValue, _ := json.Marshal(ga)
+	apiUrl, err := url.Parse(s.opts.ApiUrl)
+
+	if err != nil {
+		return err
+	}
+	annotationApi := *apiUrl
+	annotationApi.Path = path.Join(apiUrl.Path, "annotations")
+	req, err := http.NewRequest("POST", annotationApi.String(), bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Errorf("Failed to create grafana annotation request: %s", err)
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", s.opts.ApiKey))
+
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = response.Body.Close()
+	}()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read response data: %v", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("request to %s has failed with error code %d : %s", s.opts.ApiUrl, response.StatusCode, string(data))
+	}
+
+	return err
+}

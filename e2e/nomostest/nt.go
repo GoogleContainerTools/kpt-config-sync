f03c73322d6a09652d7ec27c848eb11f634388e8
Sam Dowell <sdowell@google.com>
2023-03-20 13:35:11 -0700

    fix port forward log for format string (#477)

--- e2e/nomostest/nt.go
+++ e2e/nomostest/nt.go
@@ -868,7 +868,7 @@ func (nt *NT) ForwardToFreePort(ns, pod, port string) (int, error) {
 		mux.Lock()
 		defer mux.Unlock()
 		cleanup = true
-		nt.T.Log("stopping port-forward %s/%s:%s process", ns, pod, port)
+		nt.T.Logf("stopping port-forward %s/%s:%s process", ns, pod, port)
 		err := cmd.Process.Kill()
 		if err != nil && !errors.Is(err, os.ErrProcessDone) {
 			nt.T.Errorf("killing port-forward %s/%s:%s process: %v", ns, pod, port, err)

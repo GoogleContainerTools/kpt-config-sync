487b05845e4eea258bac05d2a6754099624adb02
Karl Isenberg <karlisenberg@google.com>
2023-05-25 19:51:49 -0700

    Add conversion-gen for configsync.gke.io/v1alpha1
    - This is required to support v1alpha1 <-> v1beta1 conversion.
    - The conversion from v1beta1 -> v1alpha1 is lossy, because
      v1alpha1 does not support specifying spec.helm.deployNamespace
    - This conversion unblocks automatic conversion to the default
      version by the fake.Client.

--- pkg/core/scheme.go
+++ pkg/core/scheme.go
@@ -62,9 +62,13 @@ func init() {
 
 	// Config Sync types
 	utilruntime.Must(clusterregistry.AddToScheme(scheme.Scheme))
+
 	utilruntime.Must(configmanagementv1.AddToScheme(scheme.Scheme))
-	utilruntime.Must(configsyncv1alpha1.AddToScheme(scheme.Scheme))
+
 	utilruntime.Must(configsyncv1beta1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(configsyncv1alpha1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(configsyncv1alpha1.RegisterConversions(scheme.Scheme))
+	utilruntime.Must(scheme.Scheme.SetVersionPriority(configsyncv1beta1.SchemeGroupVersion, configsyncv1alpha1.SchemeGroupVersion))
 
 	// Hub/Fleet types
 	utilruntime.Must(hubv1.AddToScheme(scheme.Scheme))

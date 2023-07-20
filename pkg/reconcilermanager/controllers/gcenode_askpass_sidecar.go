cc1fe3dc8a9403f35f1e0d6ddc4c42fe46f8d01b
haiyanmeng <haiyanmeng@google.com>
2023-07-20 19:20:25 -0400

    Bump askpass image (#755)
    This is to pick up the vulnerability fix in http://tg/1958866.
    
    b/291841151

--- pkg/reconcilermanager/controllers/gcenode_askpass_sidecar.go
+++ pkg/reconcilermanager/controllers/gcenode_askpass_sidecar.go
@@ -24,7 +24,7 @@ import (
 const (
 	// The GCENode* values are interpolated in the prepareGCENodeSnippet function
 	// Keep the image tag consistent with nomos-operator.
-	gceNodeAskpassImageTag = "v1.0.2"
+	gceNodeAskpassImageTag = "v1.0.3"
 	// GceNodeAskpassSidecarName is the container name of gcenode-askpass-sidecar.
 	GceNodeAskpassSidecarName = "gcenode-askpass-sidecar"
 	// gceNodeAskpassPort is the port number of the askpass-sidecar container.

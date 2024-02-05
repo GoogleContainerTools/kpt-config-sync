44b4dcc4d31f978edf752feeabd2833784db4331
Nan Yu <nanyu@google.com>
2024-02-05 23:59:43 +0000

    Add required IAM settings for the test infra (#1127)
    The new k8sserviceaccount requires reader permissions to be granted to
    the KSA, along with the workload identity pool. This commit adds IAM
    settings as a one-off config.

--- e2e/testinfra/terraform/common/service_accounts.tf
+++ e2e/testinfra/terraform/common/service_accounts.tf
@@ -78,3 +78,15 @@ resource "google_service_account_iam_member" "e2e_metric_writer_gke_binding" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${data.google_project.project.project_id}.svc.id.goog[config-management-monitoring/default]"
 }
+
+# Grant source reader permissions to the RootSync's KSA with GKE workload identity.
+resource "google_project_iam_member" "root-reconciler-wi-sa-iam" {
+  for_each = toset([
+    "roles/source.reader",
+    "roles/artifactregistry.reader",
+    "roles/storage.objectViewer",
+  ])
+  role    = each.value
+  member             = "serviceAccount:${data.google_project.project.project_id}.svc.id.goog[config-management-system/root-reconciler]"
+  project = data.google_project.project.id
+}

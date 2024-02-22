776db477045bbc672a61a2228c7cc3625176388b
Karl Isenberg <karlisenberg@google.com>
2024-02-22 10:51:13 -0800

    chore: Add subnets in us-central2

--- e2e/testinfra/terraform/prow/networks.tf
+++ e2e/testinfra/terraform/prow/networks.tf
@@ -117,3 +117,102 @@ resource "google_compute_subnetwork" "e2e-subnetwork-11" {
   description = "Subnetwork for use in e2e test clusters"
   private_ip_google_access = false
 }
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-1" {
+  name          = "prow-e2e-subnetwork-usc2-1"
+  ip_cidr_range = "10.1.0.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-2" {
+  name          = "prow-e2e-subnetwork-usc2-2"
+  ip_cidr_range = "10.1.16.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-3" {
+  name          = "prow-e2e-subnetwork-usc2-3"
+  ip_cidr_range = "10.1.32.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-4" {
+  name          = "prow-e2e-subnetwork-usc2-4"
+  ip_cidr_range = "10.1.48.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-5" {
+  name          = "prow-e2e-subnetwork-usc2-5"
+  ip_cidr_range = "10.1.64.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-6" {
+  name          = "prow-e2e-subnetwork-usc2-6"
+  ip_cidr_range = "10.1.80.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-7" {
+  name          = "prow-e2e-subnetwork-usc2-7"
+  ip_cidr_range = "10.1.96.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-8" {
+  name          = "prow-e2e-subnetwork-usc2-8"
+  ip_cidr_range = "10.1.112.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-9" {
+  name          = "prow-e2e-subnetwork-usc2-9"
+  ip_cidr_range = "10.1.128.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-10" {
+  name          = "prow-e2e-subnetwork-usc2-10"
+  ip_cidr_range = "10.1.144.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "e2e-subnetwork-usc2-11" {
+  name          = "prow-e2e-subnetwork-usc2-11"
+  ip_cidr_range = "10.1.160.0/20"
+  region        = "us-central2"
+  network       = google_compute_network.e2e-network.id
+  description = "Subnetwork for use in e2e test clusters"
+  private_ip_google_access = false
+}

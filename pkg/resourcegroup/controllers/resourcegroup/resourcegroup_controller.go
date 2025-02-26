9b3744ad0632784831ac45dda846ec3a60e8f35f
Karl Isenberg <karlisenberg@google.com>
2025-02-25 16:54:03 -0800

    fix: ResourceGroup resource reconcile status
    - Fixed a bug that caused the ResourceGroup
      `status.resourceStatuses[*].reconcile` field to become stale
      between apply attempts and no longer reflect the current
      reconciliation status of the managed objects.
    
    For example, when actuation and reconcile succeed, but then the
    object is rescheduled or becomes unhealthy, the reconcile status
    should be changed to reflect that by the RGC, but it wasn't, until
    the applier updated all the resource statuses in the next apply
    attempt.

--- pkg/resourcegroup/controllers/resourcegroup/resourcegroup_controller.go
+++ pkg/resourcegroup/controllers/resourcegroup/resourcegroup_controller.go
@@ -320,16 +320,26 @@ func (r *reconciler) computeStatus(
 		}
 
 		// Keep the existing object statuses written by cli-utils
-		aStatus, exists := actuationStatuses[resStatus.ObjMetadata]
-		if exists {
-			resStatus.Actuation = aStatus.Actuation
+		if aStatus, exists := actuationStatuses[resStatus.ObjMetadata]; exists {
 			resStatus.Strategy = aStatus.Strategy
+			resStatus.Actuation = aStatus.Actuation
 			resStatus.Reconcile = aStatus.Reconcile
+
+			// Update the reconcile status based on the Strategy & Actuation
+			// from the last apply attempt, and the newly computed kstatus.
+			if reconcile, err := UpdateReconcileStatusToReflectKstatus(resStatus); err != nil {
+				// Keep existing Reconcile status
+				log.Error(err, "Resource object status unknown: failed to compute")
+			} else {
+				resStatus.Reconcile = reconcile
+			}
 
 			// Update the status field to reflect source -> spec -> status,
 			// not just spec -> status.
 			resStatus.Status = UpdateStatusToReflectActuation(resStatus)
 		}
+
+		log.V(5).Info("Resource object status computed", "status", resStatus)
 
 		// add the resource status into resgroup
 		statuses = append(statuses, resStatus)
@@ -340,6 +350,73 @@ func (r *reconciler) computeStatus(
 		metrics.RecordPipelineError(ctx, nn, readinessComponent, hasErr)
 	}
 	return statuses
+}
+
+// UpdateReconcileStatusToReflectKstatus uses the current Strategy and Actuation
+// status from the applier and the newly computed kstatus to compute the
+// Reconcile status. Returns an error if one of the inputs is invalid.
+func UpdateReconcileStatusToReflectKstatus(status v1alpha1.ResourceStatus) (v1alpha1.Reconcile, error) {
+	switch status.Strategy {
+	case v1alpha1.Apply:
+		switch status.Actuation {
+		case v1alpha1.ActuationSucceeded:
+			switch status.Status {
+			case v1alpha1.Current:
+				return v1alpha1.ReconcileSucceeded, nil // Strategy=Apply + Actuation=Succeeded + kstatus=Current => Reconcile=Succeeded
+			case v1alpha1.InProgress:
+				return v1alpha1.ReconcilePending, nil // Strategy=Apply + Actuation=Succeeded + kstatus=InProgress => Reconcile=Pending
+			case v1alpha1.Failed:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Apply + Actuation=Succeeded + kstatus=Failed => Reconcile=Failed
+			case v1alpha1.Terminating:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Apply + Actuation=Succeeded + kstatus=Terminating => Reconcile=Failed // TODO: Should this be Pending?
+			case v1alpha1.NotFound:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Apply + Actuation=Succeeded + kstatus=NotFound => Reconcile=Failed // TODO: Should this be Pending?
+			case v1alpha1.Unknown:
+				// Keep the old Reconcile status
+				return status.Reconcile, nil // Strategy=Apply + Actuation=Succeeded + kstatus=Unknown => Reconcile=???
+			default: // Invalid kstatus
+				return "", fmt.Errorf("invalid kstatus: %s", status.Status)
+			}
+		case v1alpha1.ActuationPending:
+			return v1alpha1.ReconcilePending, nil // Strategy=Apply + Actuation=Pending + kstatus=ANY => Reconcile=Pending
+		case v1alpha1.ActuationSkipped:
+			return v1alpha1.ReconcileSkipped, nil // Strategy=Apply + Actuation=Skipped + kstatus=ANY => Reconcile=Skipped
+		case v1alpha1.ActuationFailed:
+			return v1alpha1.ReconcileSkipped, nil // Strategy=Apply + Actuation=Failed + kstatus=ANY => Reconcile=Skipped
+		default: // Invalid Actuation status
+			return "", fmt.Errorf("invalid actuation status: %s", status.Actuation)
+		}
+
+	case v1alpha1.Delete:
+		switch status.Actuation {
+		case v1alpha1.ActuationSucceeded:
+			switch status.Status {
+			case v1alpha1.Current:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Delete + Actuation=Succeeded + kstatus=Current => Reconcile=Failed // TODO: Should this be Pending?
+			case v1alpha1.InProgress:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Delete + Actuation=Succeeded + kstatus=InProgress => Reconcile=Failed // TODO: Should this be Pending?
+			case v1alpha1.Failed:
+				return v1alpha1.ReconcileFailed, nil // Strategy=Delete + Actuation=Succeeded + kstatus=Failed => Reconcile=Failed // TODO: Should this be Pending?
+			case v1alpha1.Terminating:
+				return v1alpha1.ReconcilePending, nil // Strategy=Delete + Actuation=Succeeded + kstatus=Terminating => Reconcile=Pending
+			case v1alpha1.NotFound:
+				return v1alpha1.ReconcileSucceeded, nil // Strategy=Delete + Actuation=Succeeded + kstatus=NotFound => Reconcile=Succeeded
+			default: // Invalid kstatus
+				return "", fmt.Errorf("invalid kstatus: %s", status.Status)
+			}
+		case v1alpha1.ActuationPending:
+			return v1alpha1.ReconcilePending, nil // Strategy=Delete + Actuation=Pending + kstatus=ANY => Reconcile=Pending
+		case v1alpha1.ActuationSkipped:
+			return v1alpha1.ReconcileSkipped, nil // Strategy=Delete + Actuation=Skipped + kstatus=ANY => Reconcile=Skipped
+		case v1alpha1.ActuationFailed:
+			return v1alpha1.ReconcileSkipped, nil // Strategy=Delete + Actuation=Failed + kstatus=ANY => Reconcile=Skipped
+		default: // Invalid Actuation status
+			return "", fmt.Errorf("invalid actuation status: %s", status.Actuation)
+		}
+
+	default: // Invalid Strategy
+		return "", fmt.Errorf("invalid actuation strategy: %s", status.Strategy)
+	}
 }
 
 // UpdateStatusToReflectActuation updates the latest computed kstatus to reflect
